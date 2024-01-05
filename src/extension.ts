086b0b134213e3d0d5828cca63a748e9422ee031
kvoon <a1176079417@qq.com>
2024-01-05 17:27:47 +0800

    feat: handler mouse event with config, add manual mode

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,53 @@
 'use strict'
 
-import { ConfigurationTarget, commands, window, workspace } from 'vscode'
-import type { ExtensionContext } from 'vscode'
+import { ConfigurationTarget, TextEditorSelectionChangeKind, commands, window, workspace } from 'vscode'
+import type { ExtensionContext, WorkspaceConfiguration } from 'vscode'
+
+enum Mode {
+  Auto = 0,
+  Manual = 1
+}
+
+function runHide(config: WorkspaceConfiguration) {
+  if (config.get('autoHideReferences'))
+    commands.executeCommand('closeReferenceSearch')
+
+  if (config.get('autoHidePanel'))
+    commands.executeCommand('workbench.action.closePanel')
+
+  if (config.get('autoHideSideBar'))
+    commands.executeCommand('workbench.action.closeSidebar')
+
+  if (config.get('autoHideAuxiliaryBar'))
+    commands.executeCommand('workbench.action.closeAuxiliaryBar')
+}
 
 export function activate(context: ExtensionContext) {
   const initialConfig = workspace.getConfiguration('autoHide')
 
-  if (initialConfig.hideOnOpen) {
-    if (initialConfig.autoHideReferences)
-      commands.executeCommand('closeReferenceSearch')
+  runHide(initialConfig)
 
-    if (initialConfig.autoHidePanel)
-      commands.executeCommand('workbench.action.closePanel')
-
-    if (initialConfig.autoHideSideBar)
-      commands.executeCommand('workbench.action.closeSidebar')
-
-    if (initialConfig.autoHideAuxiliaryBar)
-      commands.executeCommand('workbench.action.closeAuxiliaryBar')
-  }
-
-  window.onDidChangeTextEditorSelection((selection) => {
+  window.onDidChangeTextEditorSelection((e) => {
     const config = workspace.getConfiguration('autoHide')
+
+    if(config.get<Mode>('mode') === Mode.Manual)
+      return
+
     const path = window.activeTextEditor.document.fileName
     const pathIsFile = path.includes('.') || path.includes('\\') || path.includes('/')
-    const scheme = selection.textEditor.document.uri.scheme
+    const scheme = e.textEditor.document.uri.scheme
 
     if (
-      selection.selections.length !== 1 // no selections or multiselections
-      || selection.selections.find(a => a.isEmpty) == null // multiselections
+      e.kind === undefined
+      || (!config.hideByMouse && e.kind === TextEditorSelectionChangeKind.Mouse)
+      || e.selections.length !== 1 // no selections or multiselections
+      || e.selections.find(a => a.isEmpty) == null // multiselections
       || !pathIsFile // The debug window editor
       || scheme === 'output' // The output window
     )
       return
 
-    if (config.autoHideReferences)
-      commands.executeCommand('closeReferenceSearch')
-
-    if (config.autoHidePanel)
-      commands.executeCommand('workbench.action.closePanel')
-
-    if (config.autoHideSideBar)
-      commands.executeCommand('workbench.action.closeSidebar')
-
-    if (config.autoHideAuxiliaryBar)
-      commands.executeCommand('workbench.action.closeAuxiliaryBar')
+    runHide(config)
   })
 
   context.subscriptions.push(
@@ -81,6 +84,52 @@ export function activate(context: ExtensionContext) {
         )
       },
     ),
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      'autoHide.toggleHideByMouse',
+      async () => {
+        const config = workspace.getConfiguration('autoHide')
+        await config.update(
+          'hideByMouse',
+          !config.get('hideByMouse'),
+          ConfigurationTarget.Workspace,
+        )
+      },
+    ),
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      'autoHide.switchToManualMode',
+      async () => {
+        const config = workspace.getConfiguration('autoHide')
+        await config.update(
+          'mode',
+          Mode.Manual,
+          ConfigurationTarget.Workspace,
+        )
+      },
+    ),
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand(
+      'autoHide.switchToAutoMode',
+      async () => {
+        const config = workspace.getConfiguration('autoHide')
+        await config.update(
+          'mode',
+          Mode.Auto,
+          ConfigurationTarget.Workspace,
+        )
+      },
+    ),
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand('autoHide.runHide', () => runHide(workspace.getConfiguration('autoHide')))
   )
 }
 
