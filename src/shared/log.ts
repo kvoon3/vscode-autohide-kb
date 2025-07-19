6e141c9cc2c215eef513ff3614211da183348130
kvoon <kvoon@duck.com>
2025-07-19 23:17:56 +0800

    feat: add logger.log and $inspect

--- src/shared/log.ts
+++ src/shared/log.ts
@@ -1,4 +1,19 @@
-import { defineLogger } from 'reactive-vscode'
+import { defineLogger, toValue, watchEffect } from 'reactive-vscode'
 import { displayName } from '../generated/meta'
 
-export const logger = defineLogger(displayName)
+const _logger = defineLogger(displayName)
+
+export const logger = {
+  ..._logger,
+  log(...args: any[]) {
+    for (const arg of args) {
+      logger.info('arg', JSON.stringify(toValue(arg), null, 2))
+    }
+  },
+}
+
+export function $inspect(...args: any[]) {
+  watchEffect(() => {
+    logger.log(...args)
+  })
+}
